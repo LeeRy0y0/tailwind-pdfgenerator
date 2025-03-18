eb78901652c17c0ee767170afc45e2f5d88c5904
LeeRy0y0 <emilleer41@gmail.com>
2025-03-18 23:42:04 +0100

    Update PdfGeneratorServiceProvider.php

--- src/PdfGeneratorServiceProvider.php
+++ src/PdfGeneratorServiceProvider.php
@@ -7,7 +7,7 @@ class PdfGeneratorServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'pdf-generator');
+        //$this->loadViewsFrom(__DIR__.'/../resources/views', 'pdf-generator');
         $this->publishes([
             __DIR__.'/../config/pdf-generator.php' => config_path('pdf-generator.php'),
         ], 'config');
