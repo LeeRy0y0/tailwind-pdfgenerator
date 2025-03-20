480e9f880d49ce21bee94faf1a8716d8ac1b5166
LeeRy0y0 <emilleer41@gmail.com>
2025-03-20 12:33:24 +0100

    Update tailwind-pdfgenerator: [brief description of changes]

--- src/PdfDocument.php
+++ src/PdfDocument.php
@@ -25,45 +25,63 @@ class PdfDocument
         $this->footerView = $footerView;
         return $this;
     }
-
-   public function output()
+    
+    public function output()
     {
         $html = View::make($this->view, $this->data)->render();
         $pdfFile = storage_path('app/' . $this->filename);
-
+    
+        // If a footer view is provided, render it and save it to a temporary file.
+        $footerTemplatePath = null;
+        if ($this->footerView) {
+            $footerHtml = View::make($this->footerView, $this->data)->render();
+            $footerTemplatePath = tempnam(sys_get_temp_dir(), 'pdf-footer-') . '.html';
+            file_put_contents($footerTemplatePath, $footerHtml);
+        }
+    
         $script = base_path('vendor/leertech/tailwind-pdfgenerator/scripts/generate-pdf.cjs');
         $node   = (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') ? 'node.exe' : 'node';
-        $cmd    = escapeshellcmd("$node $script " . escapeshellarg($pdfFile));
-
+    
+        // Include the footer template path if it exists
+        $cmd = escapeshellcmd($node . ' ' . $script . ' ' . escapeshellarg($pdfFile));
+        if ($footerTemplatePath) {
+            $cmd .= ' ' . escapeshellarg($footerTemplatePath);
+        }
+    
         $descriptors = [
             0 => ['pipe', 'r'],  // STDIN
             1 => ['pipe', 'w'],  // STDOUT
             2 => ['pipe', 'w'],  // STDERR
         ];
-
+    
         $process = proc_open($cmd, $descriptors, $pipes);
-
+    
         if (!is_resource($process)) {
             throw new \RuntimeException('Kunne ikke starte Node-processen');
         }
-
+    
         fwrite($pipes[0], $html);
         fclose($pipes[0]);
-
+    
         $stderr = stream_get_contents($pipes[2]);
         fclose($pipes[2]);
-
+    
         $return = proc_close($process);
-
+    
+        // Clean up the temporary footer file if it was created
+        if ($footerTemplatePath && file_exists($footerTemplatePath)) {
+            unlink($footerTemplatePath);
+        }
+    
         if ($return !== 0) {
             \Log::error("PDF generation fejl: $stderr");
             throw new \RuntimeException("PDF generation mislykkedes: $stderr");
         }
-
+    
         if ($this->download) {
             return response()->download($pdfFile)->deleteFileAfterSend(true);
         }
-
+    
         return $pdfFile;
-    }
+    }    
 }
